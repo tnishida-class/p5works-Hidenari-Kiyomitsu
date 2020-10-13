2c725375dea75210964ad9ff008645f7c0216407
Hidenari-Kiyomitsu <61923026+Hidenari-Kiyomitsu@users.noreply.github.com>
2020-10-13 22:17:33 +0900

    change triangle size

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -7,10 +7,10 @@ function draw() {
   strokeWeight(4);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(94, 85, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(8);
   textFont("serif");
-  text("23", 68, 100);
+  text("カタカナケヤキ", 68, 100);
 }
