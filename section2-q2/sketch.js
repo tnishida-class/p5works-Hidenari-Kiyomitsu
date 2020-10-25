73e48b77f10a5503e0f6fea60b36467af8309dea
Hidenari-Kiyomitsu <61923026+Hidenari-Kiyomitsu@users.noreply.github.com>
2020-10-25 15:20:59 +0900

    ocero

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,21 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(j % 2 == 0){
+        fill(i % 2 == 1 ? 125 : 255);
+      }
+      else if(j % 2 == 1){
+        fill(i % 2 == 1 ? 255 : 125);
+      }
+      rect(size * i, size * j, size, size);
     }
+    // for(let j = 0; j < 3; j++){
+    //   if(j % 2 == 0){
+    //     fill(i % 2 == 1 ? 'red' : 255);
+    //   }
+    //   else if(j % 2 == 1){
+    //     fill(i % 2 == 0 ? 'red' : 255);
+    //   }
+    // }
   }
 }
